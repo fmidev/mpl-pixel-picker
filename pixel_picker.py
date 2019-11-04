f4dc6ebbe15a7717b96f8335ae0c7a120c94ab3d
Joona Laine <joona.laine@spatineo.com>
2019-11-04 14:23:47 +0200

    #3: Use Rectangle patches instead of line to cover the whole pixel

--- pixel_picker.py
+++ pixel_picker.py
@@ -1,63 +1,62 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle
+
+# When plotting in matplotlib the upper left corner has the values of:
+Y_OFFSET = - 0.5
+X_OFFSET = -0.5
 
 
 class PixelPicker:
-    def __init__(self, line, ax, mouse_button):
-        self.line = line
-        self.ax = ax
+    def __init__(self, figure, rect_coll, mouse_button):
+        self.rects = set()
+        self.figure = figure
+        self.rect_coll = rect_coll
         self.mouse_button = mouse_button
-        self.xs = list(line.get_xdata())
-        self.ys = list(line.get_ydata())
-        self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
-        self.cidmotion = self.line.figure.canvas.mpl_connect('motion_notify_event', self._on_motion)
-        ax.callbacks.connect('xlim_changed', self._on_xlims_change)
-        ax.callbacks.connect('ylim_changed', self._on_ylims_change)
+        self.cid = figure.canvas.mpl_connect('button_press_event', self)
+        self.cidmotion = figure.canvas.mpl_connect('motion_notify_event', self._on_motion)
 
     def __call__(self, event):
         # print('click', event)
-        if event.inaxes != self.line.axes or event.button != self.mouse_button: return
-        self._draw_dot(event)
-
-    def _on_xlims_change(self, axes):
-        print("x_lim changed", self.ax.get_xlim())
-
-    def _on_ylims_change(self, axes):
-        print("y_lim canhed", self.ax.get_ylim())
+        if event.inaxes != self.figure.get_axes()[0] or event.button != self.mouse_button: return
+        self._add_rectangle(event)
 
     def _on_motion(self, event):
         # print('motion', event)
-        if event.inaxes != self.line.axes or event.button != self.mouse_button: return
-        self._draw_dot(event)
+        if event.inaxes != self.figure.get_axes()[0] or event.button != self.mouse_button: return
+        self._add_rectangle(event)
 
-    def _draw_dot(self, event):
-        self.xs.append(int(event.xdata))
-        self.ys.append(int(event.ydata))
-        self.line.set_data(self.xs, self.ys)
-        self.line.figure.canvas.draw()
+    def _add_rectangle(self, event):
+        xy = (int(round(event.xdata)) + X_OFFSET, int(round(event.ydata)) + Y_OFFSET)
+        size_before = len(self.rects)
+        self.rects.add(Rectangle(xy, 1, 1))
+        if len(self.rects) > size_before:
+            self.rect_coll.set_paths(self.rects)
+            self.figure.canvas.draw()
 
     def get_pixels(self):
-        return list(set([(self.xs[i], self.ys[i]) for i in range(len(self.xs))]))
+        return [(x - X_OFFSET, y - Y_OFFSET) for x, y in map(lambda r: r.xy, self.rects)]
 
 
-def pick_pixels(class_num=0, color=(1, 0, 0, 0.8), marker="s", mouse_button=3):
+def pick_pixels(class_num=0, color=(1, 0, 0, 0.7), mouse_button=3):
     """
 
     :param class_num: Number of the class
     :param color: Color of the painted pixels
-    :param marker: Marker symbol
     :param mouse_button: 1 = left click, 3 = right click
-    :return: Pixel x and y coordinates as tuples inside a list
+    :return: Class number and after that pixel x and y coordinates as tuples inside a list
     """
     fig = list(map(plt.figure, plt.get_fignums()))[0]
     ax = fig.get_axes()[0]  # plt.gca()
-    line, = ax.plot([], [], linestyle="none", marker=marker, color=color)
-    picker = PixelPicker(line, ax, mouse_button)
+    rect_coll = PatchCollection([], facecolors=color, edgecolors=color)
+    ax.add_collection(rect_coll)
+    picker = PixelPicker(fig, rect_coll, mouse_button)
     input("Press enter to stop picking pixels")
     return [class_num] + picker.get_pixels()
 
 
-def generate_random_image(loc, title, h=200, w=200, old_ax=None):
+def generate_random_image(loc, title, h=60, w=60, old_ax=None):
     img = (np.random.standard_normal([h, w, 3]) * 255).astype(np.uint8)
     ax = plt.subplot(loc) if old_ax is None else plt.subplot(loc, sharex=old_ax, sharey=old_ax)
     ax.set_adjustable('box')
